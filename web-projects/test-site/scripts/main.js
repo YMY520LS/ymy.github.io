7d8384e9405e1d584a6e5c464cfa32eb865968a6
YMY520LS <132960746+YMY520LS@users.noreply.github.com>
2024-02-07 00:10:16 +0800

    Add files via upload

---
+++ web-projects/test-site/scripts/main.js
@@ -0,0 +1,32 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/AUST.png") {
+    myImage.setAttribute("src", "images/AUST2.png");
+  } else {
+    myImage.setAttribute("src", "images/AUST.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+        setUserName();
+      } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "欢迎：" + myName;
+      }
+    }  
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "安徽理工大学";
+  }
+myButton.onclick = function () {
+    setUserName();
+  };
+  
+  
